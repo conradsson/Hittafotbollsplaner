9ea0535fe32d1f71e15c4796f8f2c3a77cd19acb
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-04-05 15:07:12 +0200

    Lägga till fotbollsplaner fungerar

--- Hittafotbollsplaner/Hittafotbollsplaner/Models/fotbollsplaner.cs
+++ Hittafotbollsplaner/Hittafotbollsplaner/Models/fotbollsplaner.cs
@@ -11,6 +11,7 @@ namespace Hittafotbollsplaner.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class fotbollsplaner
     {
