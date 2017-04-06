c73f25f4ecfb50517c1807b9149f006c1f3f3ec1
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-04-06 09:35:16 +0200

    Validering i lägg till fixat

--- Hittafotbollsplaner/Hittafotbollsplaner/Models/fotbollsplaner.cs
+++ Hittafotbollsplaner/Hittafotbollsplaner/Models/fotbollsplaner.cs
@@ -16,10 +16,16 @@ namespace Hittafotbollsplaner.Models
     public partial class fotbollsplaner
     {
         public int Id { get; set; }
+        [Required, StringLength(25, MinimumLength = 3, ErrorMessage = "Namnet bör vara minst tre bokstäver.")]
         public string Namn { get; set; }
+        [Required, StringLength(25, MinimumLength =3, ErrorMessage ="Adressen bör vara minst tre bokstäver.")]
         public string Adress { get; set; }
+        [Required, StringLength(20, MinimumLength = 3, ErrorMessage = "Ortsnamnet bör vara minst tre bokstäver.")]
         public string Ort { get; set; }
+        [Required, StringLength(5, MinimumLength = 5, ErrorMessage = "Postnummret bör vara fem siffor ihop.")]
         public string Postnummer { get; set; }
+        [Required, StringLength(20, MinimumLength = 4, ErrorMessage = "Vänligen fyll i rätt underlag.")]
         public string Underlag { get; set; }
+
     }
 }
