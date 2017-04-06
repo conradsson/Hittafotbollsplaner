fada19c07ad12475e773868b444c0f80fcb3cbf3
Johan Conradsson <johanconradsson@outlook.com>
2017-04-06 13:30:44 +0200

    snackbar

--- Hittafotbollsplaner/Hittafotbollsplaner/Scripts/site.js
+++ Hittafotbollsplaner/Hittafotbollsplaner/Scripts/site.js
@@ -1,1 +1,5 @@
-﻿
+﻿function snackbarFunc() {
+    var x = document.getElementById("snackbar")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
