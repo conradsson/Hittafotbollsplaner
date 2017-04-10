2459dd4a48871279333f3c9a6ab2e89a36844a68
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-04-10 11:35:57 +0200

    autocomplete fungerar

---
+++ Hittafotbollsplaner/Hittafotbollsplaner/Models/FotbollsplanerAutocomplete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hittafotbollsplaner.Models
+{
+    public class FotbollsplanerAutocomplete
+    {
+        public string Namn { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Value { get; set; }
+    }
+}
--- Hittafotbollsplaner/Hittafotbollsplaner/Scripts/site.js
+++ Hittafotbollsplaner/Hittafotbollsplaner/Scripts/site.js
@@ -4,3 +4,6 @@
     });
     jQuery('.modal').modal('hide')
 });
+
+
+
