2807fc00720dbf2cec1b57c186d20bfad7c740b0
Johan Conradsson <johanconradsson@outlook.com>
2017-05-10 11:38:15 +0200

    Nya routes

--- Hittafotbollsplaner/Hittafotbollsplaner/App_Start/RouteConfig.cs
+++ Hittafotbollsplaner/Hittafotbollsplaner/App_Start/RouteConfig.cs
@@ -13,11 +13,12 @@ namespace Hittafotbollsplaner
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(name: "Default",url: "{controller}/{id}",defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+            routes.MapRoute(name: "Start", url: "", defaults: new { controller = "Home", action = "Index" });
+            routes.MapRoute(name: "Lägg till fotbollsplan", url: "laggtillfotbollsplan", defaults: new { controller = "fotbollsplaners", action = "Create" });
+            routes.MapRoute(name: "Om", url: "omoss", defaults: new { controller = "Home", action = "Om" });
+            routes.MapRoute(name: "Kontakt", url: "kontakt", defaults: new { controller = "Home", action = "Kontakt" });
+            routes.MapRoute(name: "Aktuellfotbollsplan", url: "Fotbollsplan/{id}", defaults: new { controller = "Fotbollsplan", action = "Planid"});
         }
     }
 }
