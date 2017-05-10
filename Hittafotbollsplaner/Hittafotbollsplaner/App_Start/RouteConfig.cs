c0cda2b14a0c3e500bbd1289f2925d3f386fd05c
Hischam El Yahiaoui <hiel1500@student.miun.se>
2017-05-10 19:51:52 +0200

    da

--- Hittafotbollsplaner/Hittafotbollsplaner/App_Start/RouteConfig.cs
+++ Hittafotbollsplaner/Hittafotbollsplaner/App_Start/RouteConfig.cs
@@ -20,6 +20,7 @@ namespace Hittafotbollsplaner
             routes.MapRoute(name: "Kontakt", url: "kontakt", defaults: new { controller = "Home", action = "Kontakt" });
             routes.MapRoute(name: "Aktuellfotbollsplan", url: "Fotbollsplan/{id}", defaults: new { controller = "Fotbollsplan", action = "Planid"});
             routes.MapRoute(name: "Admin", url: "admin", defaults: new { controller = "fotbollsplaners", action = "Index" });
+            routes.MapRoute(name: "login", url: "login", defaults: new { controller = "Account", action = "Register" });
         }
     }
 }
